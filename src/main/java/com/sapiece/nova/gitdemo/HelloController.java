163378d88e92b238b2c3e786e6c08c0b59513444
mr.lei <101876462+leileiya1@users.noreply.github.com>
2025-11-15 23:21:50 +0800

    Update HelloController.java

--- src/main/java/com/sapiece/nova/gitdemo/HelloController.java
+++ src/main/java/com/sapiece/nova/gitdemo/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    // 我在远程改的
     @GetMapping("/")
     public String hello() {
         return "Hello Controller";
